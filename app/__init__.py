bb6fa9eab61f8726529011b7d61284d83d0a11e7
mayur <MA20020854@wipro.com>
2019-03-26 20:58:43 +0530

    Added login support with forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app_instance = Flask(__name__)
+app_instance.config.from_object(Config)
 
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app_instance as api
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @api.route("/")
 @api.route("/index")
@@ -11,12 +12,18 @@ def index():
 	{"author" : {"name" : "lola_second_author"}, "body" : "aother book"}
 	]
 
-
 	return render_template("index.html", title = "Home", user = user, posts = posts)
 
 
 
+@api.route("/login", methods = ["POST", "GET"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():		#fails if user do get request to login page like without clicking submit / or any validation on field fails
+		flash("Login success by user {}, remember {}".format(form.name.data, form.remember.data))
+		return redirect(url_for("index"))
 
+	return render_template("/login.html", title = "sign in form", form = form)		#login failed -> try again
 
 
 
