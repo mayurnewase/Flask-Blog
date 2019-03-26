7dd2703e6f4ec5f5cef12cfdd03b40d098327e8c
mayur <MA20020854@wipro.com>
2019-03-26 18:39:33 +0530

    v2 added base.html user and post display in index function

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
 from app import app_instance as api
+from flask import render_template
 
 @api.route("/")
 @api.route("/index")
 def index():
-	return "hello"
+	user = {"name" : "lola"}
+
+	posts = [
+	{"author" : {"name" : "lola_author"}, "body" : "lola wrote a book"},
+	{"author" : {"name" : "lola_second_author"}, "body" : "aother book"}
+	]
+
+
+	return render_template("index.html", title = "Home", user = user, posts = posts)
+
 
 
 
